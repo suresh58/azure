281f845fa6785105d2bfd1fd9de8679054b3fa8c
Suresh <suresh57758@gmail.com>
2019-10-18 21:45:23 -0400

    updating 1st

--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ resource "azurerm_kubernetes_cluster" "example" {
     vm_size         = "Standard_D1_v2"
     os_type         = "Linux"
     os_disk_size_gb = 30
+    os_disk_size_gb = 30
   }
 
   service_principal {
